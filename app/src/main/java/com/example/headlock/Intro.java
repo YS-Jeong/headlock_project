dac27826fb1b5aa1271e1886868ae8343c1e3985
doyeon <raisin_9@naver.com>
2021-06-28 18:38:49 +0900

    카카오 지도 API
    카카오 api 지도

--- app/src/main/java/com/example/headlock/Intro.java
+++ app/src/main/java/com/example/headlock/Intro.java
@@ -22,5 +22,7 @@ public class Intro extends AppCompatActivity {
                 finish();
             } 
         },1000);
+
     }
+
 }
--- app/src/main/java/com/example/headlock/MainActivity.java
+++ app/src/main/java/com/example/headlock/MainActivity.java
@@ -7,12 +7,17 @@ import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -22,8 +27,12 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import net.daum.mf.map.api.MapView;
+
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 import app.akexorcist.bluetotohspp.library.BluetoothState;
@@ -254,6 +263,26 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
 
 //    public boolean onLongClick(View v) {
 //        if (v.getId() == R.id.h_btnConnect) {
@@ -284,6 +313,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+
+
 
 
 
